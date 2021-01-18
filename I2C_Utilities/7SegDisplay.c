64c737b31857b511f0811e64d715c19fe86871d2
Cameron Lane <cam@cameronlane.dev>
2021-01-18 01:00:14 -0800

    Broken, need to repair pointer cast issue in WriteTo7Seg function.

--- I2C_Utilities/7SegDisplay.c
+++ I2C_Utilities/7SegDisplay.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>			//Needed for I2C port
 #include <fcntl.h>			//Needed for I2C port
 #include <sys/ioctl.h>			//Needed for I2C port
@@ -7,27 +8,7 @@
 
 int main(){
 
-  int file_i2c;
-  int length;
-  unsigned char buffer[60] = {0};
-
-	
-  //----- OPEN THE I2C BUS -----
-  char *filename = (char*)"/dev/i2c-1";
-  if ((file_i2c = open(filename, O_RDWR)) < 0) {
-    //ERROR HANDLING: you can check errno to see what went wrong
-    printf("ALPHA: Failed to open the i2c bus");
-    return(-1);
-  }
-	
-  int addr = 0x30;          //<<<<<The I2C address of the slave
-  if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
-  {
-    printf("Failed to acquire bus access and/or talk to slave.\n");
-    //ERROR HANDLING; you can check errno to see what went wrong
-    return(-1);
-  }
-	
+  unsigned char *buffer = malloc(sizeof(char));
 	/*
 	//----- READ BYTES -----
 	length = 4;			//<<< Number of bytes to read
@@ -43,56 +24,87 @@ int main(){
 
 	
 	//----- WRITE BYTES -----
-  addr = 0x70;
+  int addr = 0x70;
 
-  if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
-  {
-    printf("Failed to acquire bus access and/or talk to slave.\n");
-    //ERROR HANDLING; you can check errno to see what went wrong
+  Clear7Seg(addr);
+
+  realloc(buffer, 5 * sizeof(char)); 
+
+  buffer[0] = 0x76;
+  buffer[1] = 0x01;
+  buffer[2] = 0x02;
+  buffer[3] = 0x03;
+  buffer[4] = 0x04;
+
+  if (WriteTo7Seg(addr, &buffer) != 0) {
     return(-1);
   }
 
-
-  buffer[0] = 0x76;
-  buffer[1] = 0x0b;
-  buffer[2] = 0x0a;
-  buffer[3] = 0x4C;
-  buffer[4] = 0x4C;
-  length = 5;			//<<< Number of bytes to write
-
-  if (write(file_i2c, buffer, length) != length) {		//write() returns the number of bytes actually written
+  /*if (write(file_i2c, buffer, length) != length) {		//write() returns the number of bytes actually written
     //if it doesn't match then an error occurred (e.g. no response from the device)
-    /* ERROR HANDLING: i2c transaction failed */
     printf("Failed to write to the i2c bus.\n");
-  }
+  }*/
  
   addr = 0x71;
 
-  if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
+/*  if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
   {
     printf("Failed to acquire bus access and/or talk to slave.\n");
     //ERROR HANDLING; you can check errno to see what went wrong
     return(-1);
-  }
+  }*/
 
+  realloc(buffer, 3 * sizeof(char));
 
   buffer[0] = 0x76;
-  buffer[1] = 0x05;
-  buffer[2] = 0x0a;
-  buffer[3] = 0x0C;
-  length = 4;			//<<< Number of bytes to write
+  buffer[1] = 0x0f;
+  buffer[2] = 0x0f;
 
-  if (write(file_i2c, buffer, length) != length) {		//write() returns the number of bytes actually written
-    //if it doesn't match then an error occurred (e.g. no response from the device)
-    /* ERROR HANDLING: i2c transaction failed */
-    printf("Failed to write to the i2c bus.\n");
+  if (WriteTo7Seg(addr, &buffer) != 0) {
+    return(-1);
   }
+
+  /*if (write(file_i2c, buffer, length) != length) {		//write() returns the number of bytes actually written
+    //if it doesn't match then an error occurred (e.g. no response from the device)
+    printf("Failed to write to the i2c bus.\n");
+  }*/
 
   return(0);
 }
 
-int WriteTo7Seg(const char address, const char buffer) {
-  //code
+int WriteTo7Seg(const int address, const char *buffer) {
+  int file_i2c;
+
+  char *filename = (char*)"/dev/i2c-1";
+  if ((file_i2c = open(filename, O_RDWR)) < 0) {
+    //ERROR HANDLING: you can check errno to see what went wrong
+    printf("In Change7SegAddress: Failed to open the i2c bus");
+    return(-1);
+  }
+
+  if (ioctl(file_i2c, I2C_SLAVE, address) < 0) {
+    printf("In Change7SegAddress: Failed to acquire bus access and/or talk to slave.\n");
+    //ERROR HANDLING; you can check errno to see what went wrong
+    return(-1);
+  }
+
+  if (write(file_i2c, buffer, (sizeof(*buffer)/sizeof(char))) != (sizeof(*buffer)/sizeof(char))) {		//write() returns the number of bytes actually written
+    //if it doesn't match then an error occurred (e.g. no response from the device)
+    /* ERROR HANDLING: i2c transaction failed */
+    printf("Failed to write to 7 Segment Display at address %c.\n", address);
+    return(-1);
+  }
+  close(file_i2c);
+  return(0);
+}
+
+int Clear7Seg(int address) {
+  unsigned char buffer = {0x76};
+  if (WriteTo7Seg(address, buffer) != 0) {
+    printf("Failed to clear 7Seg at address %d.\n", address);
+    return(-1);
+  }
+  return(0);
 }
 
 int Change7SegAddress(int old_address, const char new_address) {
--- I2C_Utilities/7SegDisplay.h
+++ I2C_Utilities/7SegDisplay.h
@@ -2,5 +2,6 @@
 #define I2CPLAYGROUND_12CUTILITIES_7SEGDISPLAY_H_
 
 int Change7SegAddress(int old_address, const char new_address);
-
+int Clear7Seg(int address);
+int WriteTo7Seg(const int address, const char buffer);
 #endif // I2CPLAYGROUND_12CUTILITIES_7SEGDISPLAY_H_
